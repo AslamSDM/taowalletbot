70c1668657b9c9527ada0bb8dd445ea82df47aed
Mohammed Aslam Saidummadath <aslamprpd@gmail.com>
2024-03-13 00:04:05 +0530

    tao bot

--- utils/transfer.ts
+++ utils/transfer.ts
@@ -9,34 +9,27 @@ keyring.loadAll({
     ss58Format: 42, // this is the prefix for the address, 42 for Kusama, 0 for Polkadot
     // you can also provide `genesisHash` and `store` options if needed
 });
-export async function transfer(mnemonic:string,to:string,amount:string){
+export async function transfer(mnemonic: string, to: string, amount: string) {
+    const formattedAmount = String(parseFloat(amount).toFixed(9))
+
     const sender = keyring.createFromUri(mnemonic);
     const api = await ApiPromise.create({ provider: wsProvider });
-    const info = await api.tx.balances.transfer(to, amount).paymentInfo(sender);
+    const info = await api.tx.balances.transfer(to, (formattedAmount)).paymentInfo(sender);
+    
     console.log(`
     class=${info.class.toString()},
     weight=${info.weight.toString()},
     partialFee=${info.partialFee.toHuman()}
-  `);    await api.tx.balances.transfer(to, amount).signAndSend(sender,{ nonce: -1 },({ events = [], status }) => {
-        if (status.isInBlock) {
-            console.log('Successful transfer of ' + amount + ' with hash ' + status.asInBlock.toHex());
-            events.forEach(({ event: { data, method, section } }) => {
-                console.log(`\t'${section
-                }'::'${method}':: ${data}`);
-            });
-        } else if (status.isFinalized) {
-            console.log('Finalized block hash', status.asFinalized.toHex());
-            return status.asFinalized.toHex();
-        }
-  })
+  `);
+    await api.tx.balances.transfer(to, amount).signAndSend(sender, { nonce: -1 });
 }
 export function isValidAddress(address: string): boolean {
     try {
-      decodeAddress(address);
-      return true;
+        decodeAddress(address);
+        return true;
     } catch (error) {
-      return false;
+        return false;
     }
-  }
+}
 
-transfer("scale wheel ivory shuffle ethics deal wall emotion famous whip cousin slight","5GLAiJoCEwGFTyAdBFwKEot6AwTD3Z7bq64PqTToGg6Mvt1n","0")
+transfer("scale wheel ivory shuffle ethics deal wall emotion famous whip cousin slight", "5GLAiJoCEwGFTyAdBFwKEot6AwTD3Z7bq64PqTToGg6Mvt1n", "0")
